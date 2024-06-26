e6d0c10af73e68d9a2cb70f1daf27d3b9c0d3872
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2024-06-25 22:57:35 -0400

    Add a new 404 page

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,13 +1,10 @@
-document.getElementById('profile-toggle-icon').addEventListener('click', function() {
+document.getElementById('profile-gif').addEventListener('click', function() {
     var gif = document.getElementById('profile-gif');
-    var icon = document.getElementById('profile-toggle-icon');
     
     if (gif.src.includes('3.gif')) {
         gif.src = '../assets/img/profile/3.png'; // Path to your still image
-        icon.src = '../assets/img/common/play.svg'; // Path to your play icon
     } else {
         gif.src = '../assets/img/profile/3.gif'; // Path to your animated GIF
-        icon.src = '../assets/img/common/pause.svg'; // Path to your pause icon
     }
 });
 
